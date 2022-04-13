32e713d0d77113d00004124bac7f5daa7364ab28
Mira128 <alexandra.miralex@gmail.com>
2022-04-14 01:19:56 +0300

    GMF-7 Download icons, flags, create enums for them

---
+++ GlideMessenger/assets/colors.ts
@@ -0,0 +1,13 @@
+export enum COLORS {
+    PINK = '#F7C0C4',
+    YELLOW = '#FFF7B2',
+    TEXT_IN_PINK = '#C05959',
+    TEXT_IN_YELLOW = '#AB733F',
+    BLUE_ICON = '#538DBE',
+    GREY_TIP = '#979D9F',
+    YELLOW_MESSAGE = '#FFFBD8',
+    PROFILE = '#ACC0DD',
+    NOTICE = '#FC5757',
+    WHITE = '#FFFFFF',
+    BLUE_BACKGROUND = '#759FC9',
+}
